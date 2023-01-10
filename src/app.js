a2478ae858fe2364aaec0a05e4a7c5b45051db15
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-01-11 01:25:08 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import ASMEgallery from "./ASMEgallery"
+
+const galleryImages = [
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  },
+  {
+    img: "http://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-2.jpg"
+  }
+];
+
+function App() {
+  return (
+      <ASMEgallery galleryImages={galleryImages} />
+  );
+}
+
+export default App;
